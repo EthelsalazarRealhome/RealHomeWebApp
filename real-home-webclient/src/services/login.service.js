c5a2cadfdf91a69619e9dcd393e9dce5c5fa5947
diegocalvo05 <00103322@uca.edu.sv>
2023-12-21 11:30:10 -0600

    add: custom hook & login service

---
+++ real-home-webclient/src/services/login.service.js
@@ -0,0 +1,22 @@
+//import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export function login ({ identifier, password }) {
+  return fetch(`${BASE_URL}/auth/login`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json" 
+    },
+    body: JSON.stringify({ identifier, password })
+  }).then(res => {
+    if(!res.ok) throw new Error("Res is not ok");
+
+    return res.json();
+  }).then(res => {
+    const { token } = res;
+
+    return token;
+  });
+}
