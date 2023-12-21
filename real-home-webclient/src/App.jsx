4c99d733fde5a9b456e3c191eb5f345c3fccf3c6
Maio Diaz <00020322@uca.edu.sv>
2023-12-21 15:36:05 -0600

    fixing app.jsx

--- real-home-webclient/src/App.jsx
+++ real-home-webclient/src/App.jsx
@@ -6,22 +6,18 @@ import OurServices from './components/Us/OurServices';
 import Moreinfo from './components/Us/Moreinfo'
 import Team from './components/Us/Team';
 import LogIn from './components/LogIn/Login';
-<<<<<<< HEAD
 import AdminHome from './components/AdminView/AdminHome';
 import MyPosts from './components/AdminView/MyPosts';
 import CreatePost from './components/AdminView/CreatePost';
 import Footer from './components/Footer';
 import LoginError from './components/LogIn/LoginError';
-
 import { UserContextProvider } from "./context/UserContext";
-=======
 import Benefits from './components/Us/Benefits';
->>>>>>> infofull
 
 function App() {
 
   return (
-<<<<<<< HEAD
+    <>
     <UserContextProvider>
       <BrowserRouter>
       <NavBar />
@@ -42,22 +38,7 @@ function App() {
       </BrowserRouter>
       <Footer/>
     </UserContextProvider>
-=======
-    <>
-    <BrowserRouter>
-    <NavBar/>
-    <Routes>
-    <Route path="/" element={<Home/>} /> 
-    <Route path="/Properties" element={<Properties/>}/>
-    <Route path="/Moreinfo" element={<Moreinfo/>} />
-    <Route path="/Team" element={<Team/>} />
-    <Route path="OurServices" element={<OurServices/>} />
-    <Route path="/LogIn" element={<LogIn/>} />
-    <Route path="/Benefits"element={<Benefits/>} />
-   </Routes>
-   </BrowserRouter>
     </>
->>>>>>> infofull
   );
 }
 
