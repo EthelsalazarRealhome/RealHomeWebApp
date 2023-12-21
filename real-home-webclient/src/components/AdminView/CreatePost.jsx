9a66a1bd534708c410753abb0f2058edc25cd818
Maio Diaz <00020322@uca.edu.sv>
2023-12-21 16:38:19 -0600

    log in responsive

--- real-home-webclient/src/components/AdminView/CreatePost.jsx
+++ real-home-webclient/src/components/AdminView/CreatePost.jsx
@@ -41,7 +41,7 @@ const CreatePost = () => {
 
     return (
 
-        <div>
+        <div className="h-[112vh]">
             <div className="max-w-2xl mx-auto p-4">
                 <h2 className="ml-[50px] text-3xl font-bold mb-6 mt-[50px]">
                     PUBLICAR PROPIEDAD
--- real-home-webclient/src/components/LogIn/Login.jsx
+++ real-home-webclient/src/components/LogIn/Login.jsx
@@ -10,31 +10,34 @@ function LogIn() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(hasLoginError) navigate("/LoginError");
+    if (hasLoginError) navigate("/LoginError");
 
-    if(isLogged) {
-      if(user?.roles?.includes(ROLES.ADMIN)) navigate("/Admin/AdminHome");
-      if(user?.roles?.includes(ROLES.SYSADMIN)) navigate("/Admin/AdminHome");
+    if (isLogged) {
+      if (user?.roles?.includes(ROLES.ADMIN)) navigate("/Admin/AdminHome");
+      if (user?.roles?.includes(ROLES.SYSADMIN)) navigate("/Admin/AdminHome");
     }
   }, [hasLoginError, isLogged, navigate, user]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     login({ identifier, password });
-  }
+  };
 
   return (
-    <section className="flex flex-col items-center justify-center h-[85vh] w-full">
+    <section className="flex flex-col items-center justify-center min-h-screen w-full">
       {isLoginLoading && <strong>Validando credenciales...</strong>}
-      {!isLoginLoading &&
-        <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-7 bg-[#042b5e] shadow-lg rounded px-8 sm:px-5 pt-6 sm:pt-3 pb-15 sm:pb-12 h-2/4 w-1/4">
+      {!isLoginLoading && (
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center justify-center gap-7 bg-[#042b5e] shadow-lg rounded px-8 sm:px-5 pt-6 sm:pt-3 pb-15 sm:pb-12 w-full max-w-md"
+        >
           <section className="flex flex-col items-center justify-center w-full my-8 gap-3">
             <label className="relative w-full">
-              <input 
-                type="text" 
+              <input
+                type="text"
                 required
-                className="px-4 h-12 w-full text-white text-l bg-inherit border-2 rounded-lg border-white border-opacity-50 outline-none hover:border-gray-600 focus:border-blue-500 focus:text-white transition duration-200 peer"  
-                onChange={e => setIdentifier(e.target.value)}
+                className="px-4 h-12 w-full text-white text-l bg-inherit border-2 rounded-lg border-white border-opacity-50 outline-none hover:border-gray-600 focus:border-blue-500 focus:text-white transition duration-200 peer"
+                onChange={(e) => setIdentifier(e.target.value)}
                 value={identifier}
               />
               <span className="text-l cursor-text text-white text-opacity-80 absolute left-0 top-3 mx-2 px-1 transition duration-200 tracking-wide peer-focus:text-blue-500 pointer-events-none peer-focus:bg-[#042b5e] peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3 ml-2 peer-valid:scale-75 peer-valid:bg-[#042b5e] peer-valid:-translate-y-6 peer-valid:-translate-x-3">
@@ -43,11 +46,11 @@ function LogIn() {
             </label>
 
             <label className="relative w-full">
-              <input 
-                type="password" 
+              <input
+                type="password"
                 required
-                className="px-4 h-12 w-full text-white text-l bg-inherit border-2 rounded-lg border-white border-opacity-50 outline-none hover:border-gray-600 focus:border-blue-500 focus:text-white transition duration-200 peer"  
-                onChange={e => setPassword(e.target.value)}
+                className="px-4 h-12 w-full text-white text-l bg-inherit border-2 rounded-lg border-white border-opacity-50 outline-none hover:border-gray-600 focus:border-blue-500 focus:text-white transition duration-200 peer"
+                onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
               <span className="text-l cursor-text text-white text-opacity-80 absolute left-0 top-3 mx-2 px-1 transition duration-200 tracking-wide peer-focus:text-blue-500 pointer-events-none peer-focus:bg-[#042b5e] peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:-translate-x-3 ml-2 peer-valid:scale-75 peer-valid:bg-[#042b5e] peer-valid:-translate-y-6 peer-valid:-translate-x-3">
@@ -63,8 +66,8 @@ function LogIn() {
             Log in
           </button>
         </form>
-      }
-    </section> 
+      )}
+    </section>
   );
 }
 
