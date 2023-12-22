d3c1531251ec3a42984f0ee9c96b5255a8e17644
Maio Diaz <00020322@uca.edu.sv>
2023-12-22 13:37:26 -0600

    hotfix menu

--- real-home-webclient/src/components/NavBar.jsx
+++ real-home-webclient/src/components/NavBar.jsx
@@ -36,9 +36,7 @@ const NavBar = () => {
           <h1
             className=" text-black font-bold text-2xl z-20"> Real State</h1>
         </NavLink>
-
         <div className='flex flex-row gap-7 z-10'>
-
           {isLogged &&
             <NavLink to="/" onClick={logout} className="font-bold text-lg cursor-pointer">
               <button className='text-white text-2xl'>
@@ -47,7 +45,11 @@ const NavBar = () => {
             </NavLink>
           }
 
-          <HiMenuAlt3 onClick={handleNav} className="bg-gray-700 z-20 text-white cursor-pointer" size={25} />
+          <div className='flex items-center'>
+            <p className='text-2xl p-1 font-bold'>Menu</p>
+            <HiMenuAlt3 onClick={handleNav} className="bg-gray-700 z-20 text-white cursor-pointer" size={25} />
+          </div>
+
           <section className={nav ? "ease-in duration-300 fixed text-gray-300 right-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10" : "absolute top-0 h-screen right-[-100%] ease-in duration-500 z-10"}>
             <ul className="flex flex-col fixed w-full h-full items-center justify-center">
               <NavLink to='/' onClick={closeNav}>
@@ -62,11 +64,12 @@ const NavBar = () => {
               {!isLogged &&
                 <NavLink to='/LogIn' onClick={closeNav}>
                   <li className="text-xl p-16">Log In for team members only*</li>
-                </NavLink>  
+                </NavLink>
               }
             </ul>
           </section>
         </div>
+
       </nav>
     </div>
   )
