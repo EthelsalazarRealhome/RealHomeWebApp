30765fb6b455b683c8e041ea667f28a31696c8e2
Maio Diaz <00020322@uca.edu.sv>
2024-02-23 12:36:17 -0600

    imgslider

--- real-home-webclient/src/components/PostView/ImgSlider.jsx
+++ real-home-webclient/src/components/PostView/ImgSlider.jsx
@@ -3,9 +3,10 @@ import img1 from '../../img/slides/slide1.jpeg';
 import img2 from '../../img/slides/slide2.jpeg';
 import img3 from '../../img/slides/slide3.jpeg';
 import img4 from '../../img/slides/slide4.jpeg';
+import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs';
 
 const ImgSlider = () => {
-  const images = [img1, img2, img3,img4]; 
+  const images = [img1, img2, img3, img4];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -20,7 +21,19 @@ const ImgSlider = () => {
   return (
     <div>
       <div>
-        <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} style={{ width: '100%', height: 'auto' }} />
+        <img
+          src={images[currentIndex]}
+          alt={`Slide ${currentIndex + 1}`}
+          style={{ width: '50%', height: 'auto', maxWidth: '100%' }}
+        />
+      </div>
+      {/**Left arrow */}
+      <div>
+        <BsChevronCompactLeft/>
+      </div>
+      {/**Right arrow */}
+      <div>
+        <BsChevronCompactRight/>
       </div>
       <button onClick={prevSlide}>Previous</button>
       <button onClick={nextSlide}>Next</button>
